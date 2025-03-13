adcc406edd8bbce186c9ea48916e332530908437
ddjangl <semmens@ualberta.ca>
2025-03-12 23:09:37 -0600

    small change for usefulness

--- ecocan/ecocar_can.c
+++ ecocan/ecocar_can.c
@@ -3,29 +3,29 @@
 
 fdcanBytes_t mapDlcToBytes(uint32_t fdcanDlc) {
   fdcanBytes_t bytes;
-  if (fdcanDlc <= FDCAN_DLC_BYTES_8) {
+  if (fdcanDlc <= 8) {
     return fdcanDlc;
   } else {
     switch (fdcanDlc) {
-    case FDCAN_DLC_BYTES_12:
+    case 9:
       bytes = FDCAN_BYTES_12;
       break;
-    case FDCAN_DLC_BYTES_16:
+    case 10:
       bytes = FDCAN_BYTES_16;
       break;
-    case FDCAN_DLC_BYTES_20:
+    case 11:
       bytes = FDCAN_BYTES_20;
       break;
-    case FDCAN_DLC_BYTES_24:
+    case 12:
       bytes = FDCAN_BYTES_24;
       break;
-    case FDCAN_DLC_BYTES_32:
+    case 13:
       bytes = FDCAN_BYTES_32;
       break;
-    case FDCAN_DLC_BYTES_48:
+    case 14:
       bytes = FDCAN_BYTES_48;
       break;
-    case FDCAN_DLC_BYTES_64:
+    case 15:
       bytes = FDCAN_BYTES_64;
       break;
     }
--- util/typedef/exported_typedef.h
+++ util/typedef/exported_typedef.h
@@ -12,3 +12,19 @@ typedef enum {
   FET_CHRGE = FUELCELL_FET | CAP_FET | RES_FET,
   FET_RUN = FUELCELL_FET | CAP_FET | RES_FET | OUT_FET,
 } fetState_t;
+
+// Typedefines for REL board state
+typedef enum {
+  ALL_RELAY_OFF = 0x00,
+  CAP_RELAY = 0x01,
+  RES_RELAY = 0x02,
+  DSCHRGE_RELAY = 0x04,
+  MTR_RELAY = 0x08,
+} relayBit_t;
+
+typedef enum {
+  RELAY_STBY = ALL_RELAY_OFF,
+  RELAY_STRTP = RES_RELAY | DSCHRGE_RELAY,
+  RELAY_CHRGE = RES_RELAY,
+  RELAY_RUN = CAP_RELAY | DSCHRGE_RELAY | MTR_RELAY,
+} rbState_t;
